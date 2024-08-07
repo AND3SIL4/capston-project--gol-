48b8dc9d0ea5f838c178650fa7b0bd0b6537a56d
AND3SIL4 <and3felipe@gmail.com>
2024-08-06 21:40:01 -0500

    build(app): get intial input from aguments passed from command line

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.print("Code is running...");
-  }
+    public static void main(String[] args) {
+        //Validate if the user passed args or not
+        if (args.length == 0) {
+            System.out.println("There is not arguments provided");
+        }
+        Map<String, String> config = validateArgs(args); //Validate if the arguments are correct
+        if (!config.isEmpty()) showArgs(config); //Show the arguments passed
+    }
+
+    //
+    //Method to validate if the
+    //
+    public static Map<String, String> validateArgs(String[] args){
+        Map<String, String> config = new HashMap<>();
+
+        for (String arg: args) {
+            String[] split = arg.split("=");
+            String result = fixArgument(split[0]);
+            config.put(result, split[1]);
+        }
+        return config;
+    }
+
+    //
+    //Method to print the args depends on validation
+    //
+    private static void showArgs(Map<String, String> config){
+        for (Map.Entry<String, String> entry : config.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+    }
+
+    //
+    //Method to know what is the variable value
+    //
+    public static String fixArgument(String arg){
+        return switch (arg) {
+            case "w" -> "width";
+            case "h" -> "height";
+            case "g" -> "generations";
+            case "s" -> "speed";
+            case "p" -> "population";
+            case "n" -> "neighborhood";
+            default -> "arg not found";
+        };
+    }
 }
