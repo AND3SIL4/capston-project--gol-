816b648766487088029b1ffdca74847bd30c7c95
AND3SIL4 <and3felipe@gmail.com>
2024-08-12 23:46:45 -0500

    todo(app): set initial values for args no present but could be by default

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import lib.Matrix;
 import lib.Validations;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,9 +14,11 @@ public class Main {
         Map<String, String> config = Config.createConfigDictionary(args);
         boolean showArgs = Validations.showArgs(config);//Validate if the arguments are correct
         if (!config.isEmpty() && showArgs) {
+            //Know the initial cell alive or dead
             String population = config.get("population");
             int n = Integer.parseInt(config.get("width"));
             int m = Integer.parseInt(config.get("height"));
+
             if (population != null && !population.trim().isEmpty()) {
                 int[][] matrix = Matrix.createMatrix(population, n, m); //initial population
                 Matrix.printMatrix(matrix);
