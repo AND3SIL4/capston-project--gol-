642bcfbef5e0aaa8a6971a18174e02499573c864
AND3SIL4 <and3felipe@gmail.com>
2024-08-14 22:27:17 -0500

    version(app): first version of capston project finished all knwoing validations are made

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,50 @@ import lib.Validations;
 
 import java.util.Map;
 
+/**
+ * This class execute the main code
+ * Validate if the args are empty and notify if an error occurred
+ */
 public class Main {
+    /**
+     * Execute the GOL project (game of life)
+     *
+     * @param args from cli in this format (w=[int] h=[int] g=[int] s=[int] p=[String] n=[int])
+     */
     public static void main(String[] args) {
         if (args.length == 0) { //Validate if the user passed args or not
             System.out.println("There is not arguments provided");
             return;
         }
+        //Store the config dictionary in a "config" variable
         Map<String, String> config = Config.createConfigDictionary(args);
         boolean showArgs = Validations.showArgs(config);//Validate if the arguments are correct
         if (!config.isEmpty() && showArgs) {
-            //Know the initial cell alive or dead
+            //Get the initial range to display the matrix
             int height = Integer.parseInt(config.get("width"));
-            int width= Integer.parseInt(config.get("height"));
+            int width = Integer.parseInt(config.get("height"));
             String population = config.get("population");
 
             //Validate for creating the matrix
             if (population != null && !population.trim().isEmpty()) {
-                int[][] matrix = Matrix.createMatrix(population, width, height);//initial population
-                int g = Integer.parseInt(config.get("generations"));
-                for (int generations = 0; generations < g; generations++) {
-                    System.out.println("Generation #" + (generations + 1));
-                    Matrix.printMatrix(matrix);
-                    Game.game(matrix);
+                try {
+                    int[][] matrix = Matrix.createMatrix(population, width, height);//initial population
+                    int g = Integer.parseInt(config.get("generations"));//Get the amount generations
+
+                    //Display the matrix and game with the rules
+                    for (int generations = 0; generations < g; generations++) {
+                        System.out.println("Generation #" + (generations + 1));
+                        Matrix.printMatrix(matrix);//Print matrix
+                        Game.game(matrix, config);//Apply rules and update the initial matrix
+
+                        //Execute speed
+                        int s = Integer.parseInt(config.get("speed"));
+                        Thread.sleep(s);
+                    }
+                } catch (Exception e) {
+                    System.out.print(e.getMessage());
                 }
             }
-        }//Show the arguments passed
+        }
     }
 }
--- src/lib/Game.java
+++ src/lib/Game.java
@@ -1,16 +1,18 @@
 package lib;
 
+import java.util.Map;
+
 public class Game {
     //Method to get the position in matrix
-    public static void game(int[][] matrix) {
+    public static void game(int[][] matrix, Map<String, String> config) {
         int[][] tempMatrix = new int[matrix.length][matrix[0].length];
 
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
                 //Checking near neighbors
-                final int i = getI(matrix, col, row);
+                final int i = getI(matrix, col, row, config);
 
-                //Apply rules
+                //Apply rules for alive cells or dead cells
                 if (matrix[row][col] == 1) {
                     boolean ruleOne = i < 2;//Less than 2 cells: will dead
                     boolean ruleTwo = i > 3;//More than 3 cells: will dead
@@ -31,14 +33,15 @@ public class Game {
         }
 
         //Copy from tempMatrix to real matrix
-        for (int row = 0; row < matrix.length ; row++) {
+        for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
                 matrix[row][col] = tempMatrix[row][col];
             }
         }
     }
 
-    private static int getI(int[][] matrix, int col, int row) {
+    private static int getI(int[][] matrix, int col, int row, Map<String, String> config) {
+        //Checking main neighbors
         int left = (col > 0) ? matrix[row][col - 1] : 0;
         int right = (col < matrix[row].length - 1) ? matrix[row][col + 1] : 0;
         int up = (row > 0) ? matrix[row - 1][col] : 0;
@@ -49,7 +52,16 @@ public class Game {
         int upRight = (row > 0 && col < matrix[row].length - 1) ? matrix[row - 1][col + 1] : 0;
         int downLeft = (row < matrix.length - 1 && col > 0) ? matrix[row + 1][col - 1] : 0;
         int downRight = (row < matrix.length - 1 && col < matrix[row].length - 1) ? matrix[row + 1][col + 1] : 0;
-        //Calculate neighbors
-        return left + right + up + down + upLeft + upRight + downLeft + downRight;
+
+        //Calculate rules depends on the neighbors
+        int n = Integer.parseInt(config.get("neighborhood"));
+        return switch (n) {
+            case 1 -> left + right + up + down;
+            case 2 -> left + right + up + down + upLeft + downRight;
+            case 3 -> left + right + up + down + upLeft + upRight + downLeft + downRight;
+            case 4 -> upLeft + upRight + downLeft + downRight;
+            case 5 ->  up + down + upLeft + upRight + downLeft + downRight;
+            default -> throw new IllegalStateException("⛔ Verify the arg n=" + n + " and try again ⛔");
+        };
     }
 }
